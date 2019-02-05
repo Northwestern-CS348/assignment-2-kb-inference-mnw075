69c5652ab0b245fe1c64ca4fde00ee75f0b6dee2
mnw075 <michawon2019@u.northwestern.edu>
2019-02-04 20:47:24 -0600

    i

--- student_code.py
+++ student_code.py
@@ -129,6 +129,99 @@ class KnowledgeBase(object):
         ####################################################
         # Student code goes here
         
+        if not isinstance(fact_or_rule, Fact) and not isinstance(fact_or_rule, Rule):
+            return
+        
+        if isinstance(fact_or_rule, Fact):
+            fact = self._get_fact(fact_or_rule)
+            if fact.supported_by:
+                print("Fact is supported.")
+                return
+            else:
+                for supported_fact in fact.supports_facts:
+                    for pair in supported_fact.supported_by:
+                        if fact in pair:
+                            kb_rule = self._get_rule(pair[1])
+                            kb_rule.supports_facts.remove(supported_fact)
+                            kb_sf = self._get_fact(supported_fact)
+                            kb_sf.supported_by.remove(pair)
+                            if not kb_sf.supported_by:
+                                self.kb_retract(kb_sf)
+                
+                for supported_rule in fact.supports_rules:
+                    for pair in supported_rule.supported_by:
+                        if fact in pair:
+                            kb_rule = self._get_rule(pair[1])
+                            kb_rule.supports_rules.remove(supported_rule)
+                            
+                            kb_sr = self._get_rule(supported_rule)
+                            kb_sr.supported_by.remove(pair)
+                            if not kb_sr.asserted:
+                                self.kb_retract(kb_sr)
+
+                self.facts.remove(fact)
+        elif isinstance(fact_or_rule, Rule):
+            rule = self._get_rule(fact_or_rule)
+            if rule.asserted:
+                print("Can't retract rule.")
+                return
+            elif not rule.supported_by:
+                for supported_fact in rule.supports_facts:
+                    for pair in supported_fact.supported_by:
+                        if rule in pair:
+                            kb_fact = self._get_fact(pair[0])
+                            kb_fact.supports_facts.remove(supported_fact)
+                            kb_sf = self._get_fact(supported_fact)
+                            kb_sf.supported_by.remove(pair)
+                            if not kb_sf.supported_by:
+                                self.kb_retract(kb_sf)
+                for supported_rule in rule.supports_rules:
+                    for pair in supported_rule.supported_by:
+                        if rule in pair:
+                            kb_fact = self._get_fact(pair[0])
+                            kb_fact.supports_facts.remove(supported_rule)
+                            kb_sr = self._get_rule(supported_rule)
+                            kb_sr.supported_by.remove(pair)
+                            if not kb_sr.asserted:
+                                self.kb_retract(kb_sr)
+                self.rules.remove(rule)                
+                
+        return
+        
+#    
+#        if isinstance(fact_or_rule, Fact):
+#            fact = self._get_fact(fact_or_rule)
+#            if not fact.supported_by:
+#                for pair in fact.supported_by:
+#                    for fr in pair[0]:
+#                        fr = self._get_fact(fr)
+#                        fr.supports_facts.remove(fact)
+#                for f in fact.supports_facts:
+#                    f = self._get_fact(f)
+#                    f.supported_by = [e for e in f.supported_by if fact not in e]
+#                    self.kb_retract(f)
+#                for r in fact.supports_rules:
+#                    r = self._get_rule(r)
+#                    r.supported_by = [e for e in r.supported_by if fact not in e]
+#                    self.kb_retract(r)
+#                self.facts.remove(fact)
+#
+#        elif isinstance(fact_or_rule, Rule):
+#            rule = self._get_rule(fact_or_rule)
+#            if not rule.asserted:
+#                for pair in rule.supported_by:
+#                    for fr in pair[1]:
+#                        fr = self._get_rule(fr)
+#                        fr.supports_rules.remove(rule)
+#                for f in rule.supports_facts:
+#                    f = self._get_fact(f)
+#                    f.supported_by = [e for e in f.supported_by if rule not in e]
+#                    self.kb_retract(f)
+#                for r in rule.supports_rules:
+#                    r = self._get_rule(r)
+#                    r.supported_by = [e for e in r.supported_by if rule not in e]
+#                    self.kb_retract(r)
+#                self.rules.remove(rule)
 
 class InferenceEngine(object):
     def fc_infer(self, fact, rule, kb):
@@ -146,3 +239,31 @@ class InferenceEngine(object):
             [fact.statement, rule.lhs, rule.rhs])
         ####################################################
         # Student code goes here
+        
+        if (not isinstance(fact, Fact)) | (not isinstance(rule, Rule)) | (not isinstance(kb, KnowledgeBase)):
+            return
+        
+        bindings = match(rule.lhs[0], fact.statement)
+        
+        if bindings:
+            if len(rule.lhs) == 1:
+                new_fact = Fact(instantiate(rule.rhs, bindings), supported_by=[[fact,rule]])
+                kb.kb_add(new_fact)
+                fact.supports_facts.append(new_fact)
+                rule.supports_facts.append(new_fact)
+            else:
+                new_lhs = []
+                for other in rule.lhs[1:]:
+                    new_lhs.append(instantiate(other, bindings)) 
+                new_rhs = instantiate(rule.rhs, bindings) 
+                new_rule = Rule([new_lhs, new_rhs], supported_by=[[fact, rule]])
+                kb.kb_add(new_rule, )
+                fact.supports_rules.append(new_rule)
+                rule.supports_rules.append(new_rule)
+        else:
+            return
+            
+            
+                
+            
+        
